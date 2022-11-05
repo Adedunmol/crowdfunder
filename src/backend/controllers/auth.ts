a1406fa1431389cefb83ec3ec4c32e900bb7d83d
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2022-11-05 21:22:08 +0100

    add auth controllers and router

---
+++ src/backend/controllers/auth.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+
+export async function registerUserController(req: Request, res: Response) {
+    
+}
+
+export async function loginUserController(req: Request, res: Response) {
+    
+}
+
+export async function logoutUserController(req: Request, res: Response) {
+    
+}
+
+export async function refreshTokenController(req: Request, res: Response) {
+    
+}
